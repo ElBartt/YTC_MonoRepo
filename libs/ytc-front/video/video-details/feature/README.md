ad52be50adad301005a9f8da69f2cb925850d773
Lip <philippe.roquesgeoffroy@gmail.com>
2023-06-03 00:22:28 +0200

    Video-details feature

---
+++ libs/ytc-front/video/video-details/feature/README.md
@@ -0,0 +1,7 @@
+# ytc-front-video-video-details-feature
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test ytc-front-video-video-details-feature` to execute the unit tests via [Vitest](https://vitest.dev/).
---
+++ libs/ytc-front/video/video-details/feature/src/lib/ytc-front-video-video-details-feature.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable-next-line */
+export interface YtcFrontVideoVideoDetailsFeatureProps {}
+
+export function YtcFrontVideoVideoDetailsFeature(props: YtcFrontVideoVideoDetailsFeatureProps) {
+  /*
+  TODO : All the page for the video details (comments, video, stats)
+  */
+  return (
+    <div>
+      {/* call <Video><Stats><CommentList> */}
+      <h1>Welcome to YtcFrontVideoVideoDetailsFeature!</h1>
+    </div>
+  );
+}
+
+export default YtcFrontVideoVideoDetailsFeature;
