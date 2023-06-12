c9d5164c8563eef4fb0f58104e23057cc01e9132
Gilles Ferrand <gilles.ferrand31@gmail.com>
2023-06-11 22:33:58 -0400

    Add stats for channel page

--- libs/shared/ui-components/src/lib/card/card-centered.tsx
+++ libs/shared/ui-components/src/lib/card/card-centered.tsx
@@ -1,28 +1,30 @@
 import { Button } from '../button/button';
 
 export interface CardCenteredProps {
-  readonly btnText: string | null;
-  readonly cardTitle: string;
-  readonly imgUrl: string;
-  readonly subTitle?: string;
-  readonly onClick: () => void;
+    readonly btnText: string | null;
+    readonly cardTitle: string;
+    readonly imgUrl: string;
+    readonly subTitle?: string;
+    readonly children?: React.ReactNode;
+    readonly onClick: () => void;
 }
 
-export function CardCentered({ btnText, imgUrl, cardTitle, subTitle, onClick }: CardCenteredProps) {
-  return (
-    <div className="card w-96 bg-base-100 shadow-xl sm:mt-3">
-      <figure className="px-10 pt-10">
-        <img referrerPolicy="no-referrer" src={imgUrl} alt="" className="max-h-20 rounded-xl" />
-      </figure>
-      <div className="card-body items-center text-center">
-        <h2 className="card-title">{cardTitle}!</h2>
-        {subTitle && <p>subTitle</p>}
-        <div className="card-actions">
-          <Button onClick={() => onClick()} customStyle="btn-primary">
-            {btnText ?? ''}
-          </Button>
+export function CardCentered({ btnText, imgUrl, cardTitle, subTitle, children, onClick }: CardCenteredProps) {
+    return (
+        <div className="card w-96 bg-base-100 shadow-xl sm:mt-3">
+            <figure className="px-10 pt-10">
+                <img referrerPolicy="no-referrer" src={imgUrl} alt="" className="max-h-20 rounded-xl" />
+            </figure>
+            <div className="card-body items-center text-center">
+                <h2 className="card-title">{cardTitle}!</h2>
+                {subTitle && <p>subTitle</p>}
+                {children}
+                <div className="card-actions">
+                    <Button onClick={() => onClick()} customStyle="btn-primary">
+                        {btnText ?? ''}
+                    </Button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
